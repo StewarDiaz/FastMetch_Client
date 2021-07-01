9ae01bf2e892c48a02dbbf3d1a6bfe9f444f4f20
StewarDiaz <jefren.3801811641@ucaldas.com>
2021-06-30 21:37:10 -0500

    30junio2021,09:35pm

---
+++ app/src/main/java/com/programmingjd/fastmetch_client/MapsFragmentClient.java
@@ -0,0 +1,107 @@
+package com.programmingjd.fastmetch_client;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsFragmentClient extends Fragment {
+
+    private GoogleMap mMap;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+
+            LatLng Mecanico1 = new LatLng(5.066328, -75.510005);
+            mMap.addMarker(new MarkerOptions().position(Mecanico1).draggable(true)
+                    .title("Jose").snippet("Mecanico del centro")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+
+
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+            LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            LocationListener locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    LatLng miUbicacion = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(miUbicacion).title("ubicacion actual"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(miUbicacion));
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(miUbicacion)
+                            .zoom(20)
+                            .bearing(1)
+                            .tilt(45
+                            )
+                            .build();
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps_client, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
---
+++ app/src/main/java/com/programmingjd/fastmetch_client/ServiceClientFragment.java
@@ -0,0 +1,66 @@
+package com.programmingjd.fastmetch_client;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.programmingjd.fastmetch_client.ApiManager.RetrofitClient;
+import com.programmingjd.fastmetch_client.adapters.ClientsAdapter;
+import com.programmingjd.fastmetch_client.adapters.DepartamentAdapater;
+import com.programmingjd.fastmetch_client.models.Client;
+import com.programmingjd.fastmetch_client.models.Departament;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class ServiceClientFragment extends Fragment {
+
+    List<Client> CliList;
+    ListView listClientIndex;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_service_client, container, false);
+
+        listClientIndex = v.findViewById(R.id.lvClients);
+
+        return v;
+    }
+
+    private void GetListFromClient(){
+        Call<List<Client>> callingClient = RetrofitClient.getInstance().getMyApy().getClientList();
+        callingClient.enqueue(new Callback<List<Client>>() {
+            @Override
+            public void onResponse(Call<List<Client>> call, Response<List<Client>> response) {
+                if(response.isSuccessful()){
+                    CliList = response.body();
+                    //aun falta hacerlo y se estalla al intentar mostrar la listView en
+                    //este fragmente
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Client>> call, Throwable t) {
+                Toast.makeText(getContext(), "error de red", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void setListAdapter(ListAdapter adapter){
+
+    }
+
+}
